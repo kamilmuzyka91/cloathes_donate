5d04847de516417b79a2a8a3974ee697e2ab2e5a
Kamil <kamil.muzyka91@gmail.com>
2020-11-13 22:29:11 +0100

    create section 3 columns and add bg

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,16 @@ import React from "react";
 
 
 import Header from "./Header";
+import Statistic from "./Statistic";
 
 
 function Home() {
-  return <Header /> 
+  return (
+    <>
+    <Header /> ,
+    <Statistic bag="10" organization="10" collection="10"/>
+    </>
+  )
 }
 
 export default Home;
---
+++ src/components/Statistic.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function Statistic({ bag, organization, collection }) {
+  return (
+    <section className="statistic container">
+      <div className="entries">
+        <div className="statistic__bag">
+          {bag}
+          <h2 className="statistic__title">oddanych worków</h2>
+          <p className="statistics__entry">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            culpa dolores, nam nihil soluta tenetur debitis quo consequuntur
+            eius, earum harum beatae repellat saepe, laudantium quisquam
+            deleniti expedita voluptatum impedit!
+          </p>
+        </div>
+
+        <div className="entries">
+          <div className="statistic__organization">
+            {organization}
+            <h2 className="statistic__title">wspartych organizacji</h2>
+            <p className="statistics__entry">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
+              a id amet dolores libero, ipsam officia blanditiis voluptas
+              quisquam ut itaque distinctio, maxime sint! Tempore, molestias.
+              Cumque quae quaerat expedita.
+            </p>
+          </div>
+        </div>
+        <div className="entries">
+          <div className="statistic__collection">
+            {collection}
+            <h2 className="statistic__title">zorganizowanych zbiórek</h2>
+            <p className="statistics__entry">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Necessitatibus, recusandae maxime, officia error doloremque nobis
+              repellat consequatur eaque ipsa dicta porro nulla ad quis expedita
+              est! At et sequi dolorum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}

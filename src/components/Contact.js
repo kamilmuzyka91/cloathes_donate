a2710d3446e5f40e353a154b8179e6df83179eeb
Kamil <kamil.muzyka91@gmail.com>
2020-11-23 22:07:55 +0100

    crate contact section in progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+import Clothes from "../assets/Background-Contact-Form.jpg";
 import Decoration from "./Decoration";
 
 class Contact extends Component {
@@ -15,43 +16,47 @@ class Contact extends Component {
   render() {
     return (
       <section className="contact" id="section_5">
+       <img className="clothes__picture" src={Clothes} alt="Clothes" />
         <div className="contact__form">
           <h1 className="contact__title">Skontaktuj się z nami</h1>
           <Decoration />
           <form onSubmit={this.handleSubmit.bind(this)} method="POST">
-            <div className="form-group">
-              <label htmlFor="name">Wpisz swoje imię</label>
-              <br />
-              <input
-                type="text"
-                className="form__control"
-                placeholder="Krzysztof"
-                value={this.state.name}
-                onChange={this.onNameChange.bind(this)}
-              />
+            <div className="form__data">
+              <div className="form">
+                <label className="form__input">Wpisz swoje imię</label>
+                <br />
+                <input
+                  type="text"
+                  className="form__input"
+                  placeholder="Krzysztof"
+                  value={this.state.name}
+                  onChange={this.onNameChange.bind(this)}
+                />
+              </div>
+              <div className="form">
+                <label className="form__input">Wpisz swój email</label>
+                <br />
 
-              <label htmlFor="inputEmail">Wpisz swój email</label>
-              <br />
-              
-              <input
-                type="email"
-                className="form__control"
-                aria-describedby="emailHelp"
-                placeholder="abc@xyz.pl"
-                value={this.state.email}
-                onChange={this.onEmailChange.bind(this)}
-              />
+                <input
+                  type="email"
+                  className="form__input"
+                  placeholder="abc@xyz.pl"
+                  value={this.state.email}
+                  onChange={this.onEmailChange.bind(this)}
+                />
+              </div>
             </div>
             <div className="contact__message">
-              <label htmlFor="message">Wpisz swoją wiadomość</label>
+              <label className="form__inputMessage">Wpisz swoją wiadomość</label>
               <textarea
-                className="form__control"
-                cols="30"
-                rows="8"
+                className="form__inputMessage"
+                cols="20"
+                rows="10"
                 placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur tenetur excepturi beatae nostrum magnam praesentium pariatur voluptatem officiis odit harum perferendis quas possimus eveniet nam, at obcaecati optio corporis temporibus?"
                 value={this.state.message}
                 onChange={this.onMessageChange.bind(this)}
               />
+              <br />
               <button type="submit" className="btn__submit">
                 Wyślij
               </button>

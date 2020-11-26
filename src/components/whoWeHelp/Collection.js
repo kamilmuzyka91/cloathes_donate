8c6768cd97a145254d9c35bc0e0ce2c5ea2c3364
Kamil <kamil.muzyka91@gmail.com>
2020-11-26 19:25:29 +0100

    correct section fundations

--- src/components/whoWeHelp/Collection.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Fundation() {
-  return <>organizacje</>;
-}
-
-export default Fundation;
--- src/components/whoWeHelp/Organization.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import WhoWeHelpNav from "./WhoWeHelpNav";
-
-function Organization() {
-  return <>komponent organizacje</>;
-}
-
-export default Organization;
